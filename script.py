f75889a87ccfad99db78747db42dbc1c60dab2a6
Meryx <anwar.haredy@gmail.com>
2024-01-13 10:52:37 +0300

    get cropped tcg images only

--- script.py
+++ script.py
@@ -2,23 +2,29 @@ import requests
 import json
 import time
 
-url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?format=tcg'
 
 response = requests.get(url)
 
 # print(response.json()['data'][0]['card_images'][0]['image_url'])
 
 num_of_requests = 0
-
-for card in response.json()['data']:
-    download_url = card['card_images'][0]['image_url']
+index = 1423
+for card in response.json()['data'][1423:]:
+    download_url = card['card_images'][0]['image_url_cropped']
     key = card['id']
+    card_type = card['type']
+    frame_type = card['frameType']
+    if frame_type == 'skill':
+        index = index + 1
+        continue
     name = card['name']
     race = card['race']
     atk = card.get('atk', 0)
     defense = card.get('def', 0)
     level = card.get('level', 0)
     attribute = card.get('attribute', 'none')
+    linkval = card.get('linkval', 'none')
 
     information = {
         'key': key,
@@ -28,6 +34,9 @@ for card in response.json()['data']:
         'defense': defense,
         'level': level,
         'attribute': attribute,
+        'linkval': linkval,
+        'type': card_type,
+        'frame_type': frame_type
     }
 
     image = requests.get(download_url)
@@ -40,4 +49,8 @@ for card in response.json()['data']:
         if num_of_requests == 19:
             time.sleep(1)
             num_of_requests = 0
+    index = index + 1
+
+with open(f"index.json", "w") as json_file:
+    json.dump(index, json_file, indent=4)
 
